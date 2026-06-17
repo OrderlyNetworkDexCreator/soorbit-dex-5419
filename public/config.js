bf144ddb84a575e92d561b87b43ab64b38c58e24
orderly-guoqing <guoqing@orderly.network>
2026-06-18 01:04:31 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -20,7 +20,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Swap,Trading,Portfolio,Leaderboard,Rewards,Vaults,Points,Campaigns,Markets",
   "VITE_CUSTOM_MENUS": "Terms & Conditions, https://soorbit.com/terms",
-  "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "true",
+  "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/soorbit_com",
   "VITE_DISCORD_URL": "https://discord.gg/soorbitcom",
